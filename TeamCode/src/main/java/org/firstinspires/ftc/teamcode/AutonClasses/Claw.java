c4ac47a700ed425a3dcedfb927f4c5e5c04a01f2
Friars23707 <147444129+Friars23707@users.noreply.github.com>
2024-10-25 12:15:08 -0400

    fixed arm to claw

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonClasses/Claw.java
@@ -0,0 +1,74 @@
+package org.firstinspires.ftc.teamcode.AutonClasses;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import java.util.List;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class Claw extends LinearOpMode {
+
+    public HardwareMap hwM;
+    public Servo claw;
+    public Servo wrist;
+
+    public Claw(HardwareMap sent_hwM) {
+        hwM = sent_hwM;
+
+        claw = hwM.get(Servo .class, "claw");
+        wrist = hwM.get(Servo.class, "wrist");
+    }
+
+    public void reset() {
+        wrist.setPosition(0.8333);
+        sleep(500);
+        claw.setPosition(0.5);
+    }
+
+    public Action collect() {
+        return new Action() {
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+
+
+                wrist.setPosition(0.5);
+                sleep(500);
+                claw.setPosition(1.0);
+                sleep(2000);
+                reset();
+
+                return true;
+            }
+        };
+    }
+
+    public Action eject() {
+        return new Action() {
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+
+
+                wrist.setPosition(0.5);
+                sleep(500);
+                claw.setPosition(1.0);
+                sleep(2000);
+                reset();
+
+                return true;
+            }
+        };
+    }
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //I want the sleep function :sob:
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Left_1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Left_1.java
@@ -3,13 +3,12 @@ package org.firstinspires.ftc.teamcode;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
-import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.teamcode.AutonClasses.Arm;
+import org.firstinspires.ftc.teamcode.AutonClasses.Claw;
 
 // RR-specific imports
 
@@ -19,24 +18,34 @@ public class Left_1 extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         //Yap yap yap
 
+        //Init most things
         Pose2d initialPose = new Pose2d(11.8, 61.7, Math.toRadians(90));
         MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
-        Arm arm = new Arm(hardwareMap);
+        Claw claw = new Claw(hardwareMap);
 
-        Action trajectoryActionChosen = drive.actionBuilder(initialPose)
+        //Create the trajs
+        Action goToBucket = drive.actionBuilder(initialPose)
                 .lineToYSplineHeading(33, Math.toRadians(0))
-                .waitSeconds(2)
-                .setTangent(Math.toRadians(90))
-                .lineToY(48)
-                .setTangent(Math.toRadians(0))
-                .lineToX(32)
-                .strafeTo(new Vector2d(44.5, 30))
-                .turn(Math.toRadians(180))
-                .lineToX(47.5)
+                .waitSeconds(3)
+                .build();
+
+        Action floorSampleOne = drive.actionBuilder(initialPose)
+                .lineToYSplineHeading(33, Math.toRadians(0))
+                .waitSeconds(3)
+                .build();
+
+        Action floorSampleTwo = drive.actionBuilder(initialPose)
+                .lineToYSplineHeading(33, Math.toRadians(0))
+                .waitSeconds(3)
+                .build();
+
+        Action floorSampleThree = drive.actionBuilder(initialPose)
+                .lineToYSplineHeading(33, Math.toRadians(0))
                 .waitSeconds(3)
                 .build();
 
 
+        //Custom Wait
         while (!isStopRequested() && !opModeIsActive()) {
             telemetry.addData("Waiting:", "0");
             telemetry.update();
@@ -48,10 +57,23 @@ public class Left_1 extends LinearOpMode {
 
         if (isStopRequested()) return;
 
+        //Run all of the actions
         Actions.runBlocking(
                 new SequentialAction(
-                        trajectoryActionChosen,
-                        arm.collect()
+                        goToBucket,
+                        claw.eject(),
+                        floorSampleOne,
+                        claw.collect(),
+                        goToBucket,
+                        claw.eject(),
+                        floorSampleTwo,
+                        claw.collect(),
+                        goToBucket,
+                        claw.eject(),
+                        floorSampleThree,
+                        claw.collect(),
+                        goToBucket,
+                        claw.eject()
                 )
         );
 
