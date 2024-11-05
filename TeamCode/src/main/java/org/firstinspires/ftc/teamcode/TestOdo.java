cbc509f6f0cd032d3bc913bf53f99116a99ba7ba
Friars23707 <147444129+Friars23707@users.noreply.github.com>
2024-11-05 14:05:17 -0500

    broken pt2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOdo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOdo.java
@@ -13,8 +13,11 @@ public class TestOdo extends LinearOpMode {
         waitForStart();
 
         while (!isStopRequested()) {
-            telemetry.addData("x: ", odo.getPosX());
-            telemetry.addData("y: ", odo.getPosY());
+            telemetry.addData("Status", "Initialized");
+            telemetry.addData("X offset", odo.getXOffset());
+            telemetry.addData("Y offset", odo.getYOffset());
+            telemetry.addData("Device Version Number:", odo.getDeviceVersion());
+            telemetry.addData("Device Scalar", odo.getYawScalar());
             telemetry.update();
         }
     }
