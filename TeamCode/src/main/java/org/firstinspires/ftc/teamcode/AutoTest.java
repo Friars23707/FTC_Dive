cb4b6bca1774b3f9dac3c7eedc58ea237d1d2b8f
Friars23707 <147444129+Friars23707@users.noreply.github.com>
2025-01-08 14:14:16 -0500

    removed rr and updated teamcode readme.md

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
@@ -2,14 +2,11 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.AutonClasses.Slide;
-import org.firstinspires.ftc.teamcode.CustomOdometry;
 
 @Autonomous
-public class Main extends LinearOpMode {
+public class AutoTest extends LinearOpMode {
     CustomOdometry customOdometry;
     Slide slide;
 
